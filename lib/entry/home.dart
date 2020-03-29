ebf345ecd1d1e1824d9c6f34eaa8220b27a3b27e
WMXPY <wm@mengw.io>
2020-03-28 21:26:45 -0500

    add home

---
+++ lib/entry/home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SeafoodCrossingHome extends StatefulWidget {
+  @override
+  _SeafoodCrossingHomeState createState() => _SeafoodCrossingHomeState();
+}
+
+class _SeafoodCrossingHomeState extends State<SeafoodCrossingHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

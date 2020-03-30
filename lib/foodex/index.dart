ec747a8b8ae4f574ad2371f7b1b7c99fc52686e9
WMXPY <wm@mengw.io>
2020-03-29 23:39:08 -0500

    fix linting

--- lib/foodex/index.dart
+++ lib/foodex/index.dart
@@ -1,6 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_sticky_header/flutter_sticky_header.dart';
 
 class FoodexIndex extends StatefulWidget {
   @override

b484193314ca7a36c4f777094f09e6978b47d288
PCXPY <pc@wmpcxpy.com>
2020-03-31 18:34:03 -0500

    [chore] init insect data

---
+++ lib/foodex/data/insect.dart
@@ -0,0 +1,641 @@
+import 'package:seafood_crossing/foodex/entity/item.dart';
+
+final List<FoodexItemEntity> insectData = [
+  FoodexItemEntity(
+    name: 'common-butterfly',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/5/58/Common_Butterfly_HHD_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'yellow-butterfly',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/d/de/Yellow_Butterfly_HHD_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'tiger-butterfly',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/9/9b/Tiger_Butterfly_HHD_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'peacock-butterfly',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/0/03/Peacock_Butterfly_HHD_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'common-bluebottle',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/b/bd/Common_Bluebottle_NH_Icon.PNG',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'paper-kite-butterfly',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/6/63/Paper_Kite_Butterfly_NH_Icon.PNG',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'great-purple-emperor',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/c/c2/Great_Purple_Emperor_NH_Icon.PNG',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'monarch-butterfly',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/d/d4/Monarch_Butterfly_HHD_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'emperor-butterfly',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/8/8d/Emperor_Butterfly_HHD_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'agrias-butterfly',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/b/b5/Agrias_Butterfly_HHD.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'rajah-brooke-birdwing',
+    icon: '',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'queen-alexandra-birdwing',
+    icon: '',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'moth',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/f/f9/Moth_HHD_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'atlas-moth',
+    icon: '',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'madagascan-sunset-moth',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/d/dd/Madagascan_Sunset_Moth_NH_Icon.PNG',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'long-locust',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/0/05/Long_Locust_HHD_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'migratory-locust',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/c/c0/Migratory_Locust_HHD_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'rice-grasshopper',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/3/3c/Rice_Grasshopper_HHD_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'grasshopper',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/4/47/Grasshopper_HHD_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'cricket',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/0/0a/Cricket_HHD_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'bell-cricket',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/b/b2/Bell_Cricket_HHD_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'mantis',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/1/1e/Mantis_HHD_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'orchid-mantis',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/6/64/Orchid_Mantis_HHD_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'honeybee',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/d/dc/Honeybee_HHD_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'wasp',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/a/ae/Bee_HHD_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'brown-cicada',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/2/21/Brown_Cicada_HHD.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'robust-cicada',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/d/d6/Robust_Cicada_HHD.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'giant-cicada',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/3/35/Giant_Cicada_HHD.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'walker-cicada',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/d/d7/Walker_Cicada_HHD_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'evening-cicada',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/5/55/Evening_Cicada_HHD.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'cicada-shell',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/7/7e/Cicada_Shell_HHD_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'red-dragonfly',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/5/5f/Red_Dragonfly_HHD_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'darner-dragonfly',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/6/6e/Darner_Dragonfly_HHD_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'banded-dragonfly',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/6/6e/Banded_Dragonfly_HHD_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'damselfly',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/6/6f/Damselfly_NH_Icon.PNG',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'firefly',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/d/d6/Firefly_NH_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'mole-cricket',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/4/4a/Mole_Cricket_HHD_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'pondskater',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/f/f3/Pondskater_HHD_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'diving-beetle',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/6/64/Diving_Beetle_HHD_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'giant-water-bug',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/b/b2/Giant_Water_Bug_NH_Icon.PNG',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'stinkbug',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/f/f3/Stinkbug_%28New_Leaf_icon%29.PNG',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'man-faced-stink-bug',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/c/c0/Man-faced_Stink_Bug_NH_Icon.PNG',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'ladybug',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/7/72/Ladybug_HHD_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'tiger-beetle',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/7/73/Tiger_Beetle_HHD_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'jewel-beetle',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/4/43/Jewel_Beetle_NH_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'violin-beetle',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/5/55/Violinbeetle.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'citrus-long-horned-beetle',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/0/03/Citrus_Long-horned_Beetle_NH.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'rosalia-batesi-beetle',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/5/58/Rosalia_Batesi_Beetle_NH_Icon.PNG',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'blue-weevil-beetle',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/2/23/Blue_Weevil_Beetle_NH_Icon.PNG',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'dung-beetle',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/c/c4/Dung_Beetle_NH_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'earth-boring-dung-beetle',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/5/51/Earth-Boring_Dung_Beetle_NH_Icon.PNG',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'scarab-beetle',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/7/7b/Scarab_Bettle_NH_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'drone-beetle',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/c/c3/Drone_Beetle_NH_Icon.PNG',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'goliath-beetle',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/3/3e/Goliath_Beetle_NH_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'saw-stag',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/0/03/Saw_Stag_NH_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'miyama-stag',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/0/0a/Miyama_Stag_NH_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'giant-stag',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/0/08/Giant_Stag_NH_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'rainbow-stag',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/f/f7/Rainbow_Stag_NH_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'cyclommatus-stag',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/a/a0/Cyclommatus_Stag_NH_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'golden-stag',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/b/ba/Golden_Stag_NH_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'giraffe-stag',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/8/8f/Giraffe_Stag_NH_Icon.PNG',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'horned-dynastid',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/0/00/Horned_Dysnastid_NH_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'horned-atlas',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/2/2a/Horned_Atlas_NH_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'horned-elephant',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/e/ea/Horned_Elephant_NH_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'horned-hercules',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/2/21/Horned_Hercules_NH_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'walking-stick',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/5/58/Walking_Stick_NH_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'walking-leaf',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/a/ab/Walking_Leaf_HHD_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'bagworm',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/0/02/Bagworm_HHD_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'ant',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/1/12/Ant_HHD_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'hermit-crab',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/1/11/Hermit_Crab_HHD_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'wharf-roach',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/7/7d/Wharf_Roach_HHD_Icon.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'fly',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/a/a2/NH-Fly.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'mosquito',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/a/ad/NH-Mosquito.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'flea',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/7/7b/NH-Flea.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'snail',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/7/7c/NH-Snail.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'pill-bug',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/6/6b/NH-Pill-Bug.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'centipede',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/1/11/NH-Centipede.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'spider',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/1/16/NH-Spider.png/revision/latest/scale-to-width-down/64?cb=20200327132028',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'tarantula',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/a/aa/NH-Tarantula.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+  FoodexItemEntity(
+    name: 'scorpion',
+    icon:
+        'https://vignette.wikia.nocookie.net/animalcrossing/images/4/48/NH-Scorpion.png',
+    northernMonths: [],
+    southernMonths: [],
+    hours: [],
+  ),
+];

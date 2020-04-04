e6d31d44f0c75bbb42ff2bfb11d6331d4c516eb8
WMXPY <wm@mengw.io>
2020-04-04 18:45:51 -0500

    implement add destination form

--- lib/travel/add-destination.dart
+++ lib/travel/add-destination.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:seafood_crossing/i18n/core/localizations.dart';
 
 class AddDestination extends StatefulWidget {
   @override
@@ -13,23 +14,23 @@ class _AddDestinationState extends State<AddDestination> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('1'),
+        title: CoreLocalizations.of(context).getText('create-destination'),
       ),
       body: ListView(
         children: <Widget>[
           Container(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.fromLTRB(12.0, 5.0, 12.0, 5.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.end,
               children: <Widget>[
                 Form(
                   key: this._formKey,
                   child: Column(
-                    children: this._buildFormFields(),
+                    children: this._buildFormFields(context),
                   ),
                 ),
                 RaisedButton(
-                  child: Text('Submit'),
+                  child: CoreLocalizations.of(context).getText('submit'),
                   onPressed: this._submit,
                 ),
               ],
@@ -40,23 +41,66 @@ class _AddDestinationState extends State<AddDestination> {
     );
   }
 
-  List<Widget> _buildFormFields() {
+  List<Widget> _buildFormFields(BuildContext context) {
+    final CoreLocalizations coreLocalizations = CoreLocalizations.of(context);
+    const String titleFieldName = 'title';
+    const String descriptionFieldName = 'description';
+    const String accessCodeFieldName = 'access-code';
+
     return <Widget>[
       TextFormField(
         decoration: InputDecoration(
-          labelText: 'username',
+          labelText: coreLocalizations.getString(titleFieldName),
+          hintText: coreLocalizations.getString(titleFieldName + '-hint'),
         ),
         onSaved: (String value) {
-          this._data['title'] = value;
+          this._data[titleFieldName] = value;
         },
         validator: (String value) {
           if (value.length > 0) {
             return null;
           }
-          return 'NPOOP';
+          return this._buildRequiredMessage(context, titleFieldName);
+        },
+      ),
+      TextFormField(
+        decoration: InputDecoration(
+          labelText: coreLocalizations.getString(descriptionFieldName),
+          hintText: coreLocalizations.getString(descriptionFieldName + '-hint'),
+        ),
+        onSaved: (String value) {
+          this._data[descriptionFieldName] = value;
+        },
+        validator: (String value) {
+          if (value.length > 0) {
+            return null;
+          }
+          return this._buildRequiredMessage(context, descriptionFieldName);
+        },
+      ),
+      TextFormField(
+        decoration: InputDecoration(
+          labelText: coreLocalizations.getString(accessCodeFieldName),
+          hintText: coreLocalizations.getString(accessCodeFieldName + '-hint'),
+        ),
+        onSaved: (String value) {
+          this._data[accessCodeFieldName] = value;
+        },
+        validator: (String value) {
+          if (value.length > 0) {
+            return null;
+          }
+          return this._buildRequiredMessage(context, accessCodeFieldName);
         },
       ),
     ];
+  }
+
+  String _buildRequiredMessage(BuildContext context, String fieldName) {
+    final CoreLocalizations coreLocalizations = CoreLocalizations.of(context);
+    return coreLocalizations.getString(fieldName) +
+        ' ' +
+        coreLocalizations.getString('is-required');
   }
 
   void _submit() {

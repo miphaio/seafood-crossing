52b90ff90d18f7b99a1b7a95866e39c66e68971e
WMXPY <wm@mengw.io>
2020-04-04 16:50:26 -0500

    add repostiroies

---
+++ lib/travel/repository/create.dart
@@ -0,0 +1,43 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/widgets.dart';
+import 'package:seafood_crossing/util/device-info.dart';
+import 'package:seafood_crossing/util/path.dart';
+import 'package:seafood_crossing/util/user-info.dart';
+
+class CreateRepositoryResponse {
+  final bool created;
+
+  CreateRepositoryResponse({
+    @required this.created,
+  });
+}
+
+Future<CreateRepositoryResponse> createRepository({
+  @required String title,
+  @required String description,
+  @required String accessCode,
+}) async {
+  final String target = joinPath(['dev', 'travel', 'destination', 'create']);
+
+  final DeviceInfo deviceInfo = await DeviceInfo.gather();
+  final UserInfo userInfo = await UserInfo.gather();
+
+  try {
+    Response response = await Dio().post(
+      target,
+      data: {
+        'identifier': userInfo.identifier,
+        'device': deviceInfo.toMap(),
+        'title': title,
+        'description': description,
+        'accessCode': accessCode,
+      },
+    );
+
+    return CreateRepositoryResponse(
+      created: response.data['created'] as bool,
+    );
+  } catch (e) {
+    return null;
+  }
+}

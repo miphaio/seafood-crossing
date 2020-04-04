eede7f4e38f1a647a9bc4ac135964ca7df9a8df8
WMXPY <wm@mengw.io>
2020-04-04 17:01:51 -0500

    improve user info class

--- lib/util/user-info.dart
+++ lib/util/user-info.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/widgets.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+const String _Seafood_Crossing_Identifier = 'seafood-crossing-identifier';
+
 class UserInfo {
   static Future<UserInfo> gather() async {
     SharedPreferences preference = await SharedPreferences.getInstance();
 
     final String identifier =
-        preference.getString('seafood-crossing-identifier');
+        preference.getString(_Seafood_Crossing_Identifier);
 
+    if (identifier == null) {
+      return UserInfo(
+        identifier: 'new-user',
+      );
+    }
+
+    return UserInfo(
+      identifier: identifier,
+    );
+  }
+
+  static Future<UserInfo> update(String identifier) async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+
+    await preferences.setString(_Seafood_Crossing_Identifier, identifier);
     return UserInfo(
       identifier: identifier,
     );

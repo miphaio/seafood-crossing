b21e2e7c65fbad131c1c778eaa59fbe7d1b63510
WMXPY <wm@mengw.io>
2020-04-08 17:16:56 -0500

    init destination category

---
+++ lib/travel/decalre/category.dart
@@ -0,0 +1,8 @@
+class DestinationCategory {
+  static const String TURNIP_BUY = "TURNIP_BUY";
+  static const String TURNIP_SELL = "TURNIP_SELL";
+  static const String TOUR = "TOUR";
+  static const String MABEL = "MABEL";
+  static const String SAHARAH = "SAHARAH";
+  static const String OTHER = "OTHER";
+}
